b5c2851b90965dc7a9dbbc8739b38b2fe44d379b
Dhabi966 <jalewis012003@yahoo.com>
2022-09-11 22:39:11 -0600

    added updates

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const ulil = require('util');
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -10,49 +9,41 @@ const questions = [
         type: 'input',
         message: 'What is your GitHub username?',
         name: 'username',
-        default: 'Dhabi966',
     },  
     {
         type: 'input',
         message: 'What is your GitHub repo?',
-        name: 'repo',
-        default: 'JimsREADMEGen',
+        name: 'repo', 
     },
     {
         type: 'input',
         message: 'What is your project title?',
         name: 'title',
-        default: 'Project Title',
     },
     {
         type: 'input',
-        message: 'Write a description of your project',
+        message: 'Write a brief description of your project',
         name: 'description',
-        default: 'Project Description',
     },
     {
         type: 'input',
         message: 'Enter installation instructions',
-        name: 'install',
-        default: 'Project Installation',
+        name: 'install',    
     },
     {
         type: 'input',
         message: 'Enter usage information',
         name: 'usage',
-        default: 'Project Usage',
     },
     {
         type: 'input',
         message: 'Enter contribution guidelines',
         name: 'contribution',
-        default: 'Project contributions',
     },
     {
         type: 'input',
         message: 'Enter test questions',
         name: 'tests',
-        default: 'Project Tests',
     },
     {
         type: 'list',
@@ -60,35 +51,23 @@ const questions = [
         choices: ['Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License'],
         name: 'license'
     }
-
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err =>
+    fs.writeFile('Generated-README.md', data, err =>
     err ? console.error(err) : 
     console.log('Success! Your README file has been generated'),
 
 // TODO: Create a function to initialize app
 function init() {
-    try {
-    const userResponses = inquirer.prompt(questions);
-        console.log("Your responses: ", userResponses);
-        console.log("Thank you for your responses! Fetching your GitHub data next...");
-    
-        // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
-        console.log("Generating your README next...")
-        const markdown = generateMarkdown(userResponses, userInfo);
-        console.log(markdown);
-    
-        // Write markdown to file
-        writeFileAsync('ExampleREADME.md', markdown);
+    return inquirer.prompt(questions).then((data) => {
+       writeToFile('Generated-README.md', generateMarkdown(data));
+});
 
-    } catch (error) {
-        console.log(error);
+})
 
-    }
-
-
+}
 // Function call to initialize app
-init()})}
+init();
+    
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,39 +2,97 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === 'MIT') {
-    return `## License
-    [![License: MIT](https://img.shields.io/badge/License-MIT-green)](https://opensource.org/licenses/MIT)`;
+    return `# License
+    [![License: MIT](https://img.shields.io/badge/License-MIT-green)]`;
 }
 if (license === 'Apache 2.0') {
-  return `## License
-  [![License: Apache 2.0](https://img.shields.io/badge/license-Apache%202.0-blue)](https://opensource.org/licenses/Apache-2.0)`;
+  return `# License
+  [![License: Apache 2.0](https://img.shields.io/badge/license-Apache%202.0-blue)]`;
 }
-if (license === '') {
-  return `## License
-  [![License: //](https://img.shields.io/badge/License-MIT-green)](https://opensource.org/licenses/MIT)`;
+if (license === 'Mozilla 2.0') {
+  return `# License
+  [![License: //](https://img.shields.io/badge/license-Mozilla%202.0-red)]`;
 }
-if (license === '') {
-  return `## License
-  [![License: //](https://img.shields.io/badge/License-MIT-green)](https://opensource.org/licenses/MIT)`;
-}
-
-
-
+if (license === 'None') {
+  return '';
 
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return `# License
+    [![License: MIT](https://opensource.org/licenses/MIT)]`;
+  }
+  if (license === 'Apache 2.0') {
+    return `# License
+    [![License: Apache 2.0](https://opensource.org/licenses/Apache-2.0)]`;
+  }
+  if (license === 'Mozilla 2.0') {
+    return `# License
+    [![License: Mozilla 2.0](https://opensource.org/licenses/MPL-2.0)]`;
+  }
+  if (license === 'None') {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+ ${renderLicenseBadge(data.license)}
+ 
+ ## Description
+ ${data.description}
+
+ ## Table of Contents
+
+-[ Title](#title)
+
+-[ Description](#description)
+
+-[ Installation](#installation)
+
+-[ Usage](#usage)
+
+-[ License](#license)
+
+-[ Contributing](#contributing)
+
+-[ Test](#tests)
+
+-[ Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+
+## License 
+${data.license}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+${data.questions}
+github.com/${data.Github}
+
+${data.Email}
+
 `;
 }
 
-module.exports = generateMarkdown;}
+module.exports = generateMarkdown;}}
